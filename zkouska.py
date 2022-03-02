9e1e388028ea8665fbcebe63a137f6f64d6d9932
Jan Králík <kralik.jan@sspbrno.cz>
2022-03-02 08:42:58 +0100

    Ahoj velice mluvene Hodne mluveni malo kodovani

--- zkouska.py
+++ zkouska.py
@@ -1,1 +1,22 @@
-print("ahoj")
+# JEDNODUCHY PROGRAM NA VYPSANI HELLO WORLD
+#
+# -----------------------------------------
+
+
+#Zacneme funkci print, do argumentu napiseme nas string v  uvozovkach!
+#Argument se nachazi v zavorkach
+#Tyto zavorky: ()
+
+# --------------------------------------------
+
+#zde naimportujeme sleep ano
+from time import sleep
+
+
+print("Hello world")
+sleep(6)
+
+
+
+# OUTPUT je pote hello world
+#Ahoj!
